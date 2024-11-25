87a9318143433909f1b5cfc00aa18a3ce98a2956
Bryan Uriel Ramirez Espinosa <bryan.ramirez9289@alumnos.udg.mx>
2024-11-24 20:20:26 -0600

    doughnut chart added

--- src/components/DoughnutChart.jsx
+++ src/components/DoughnutChart.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
+ChartJS.register(ArcElement, Tooltip);
 
 export const DoughnutChart = () => {
    
+    const data = {
+        labels: ['Label 1', 'Label 2', 'Label 3'],
+        datasets: [
+          {
+            data: [30, 50, 20],
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          },
+        ],
+      };
+    
+      const options = {
+        // You can customize chart options here
+      };
+
     return (
         <>
-            <Doughnut 
-                data={{
-                    labels: ["A", "B"],
-                    datasets: [
-                        {
-                            label: "cosa1",
-                            data:[100, 200],
-                            backgroundColor: ["black", "white"],
-                            borderRadius:5
-                        }
-                    ]
-                }} 
-            ></Doughnut>
+            <Doughnut data={data} options={options} />
         </>
     )
 }
